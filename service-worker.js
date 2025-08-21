9f743f65bc91c4770232630f2f7ef540b145736c
MACGalaviz <MACGalaviz@hotmail.com>
2025-08-21 17:38:09 -0600

    :tada: New Try

--- service-worker.js
+++ service-worker.js
@@ -1,185 +1,153 @@
-const CACHE_NAME = 'lottery-cache-v10'; // Cambia el número de versión para forzar la actualización
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://unpkg.com/react@18/umd/react.development.js',
-    'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
-    'https://unpkg.com/@babel/standalone/babel.min.js',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
-    'https://placehold.co/200x200/99f6e4/1d4ed8?text=Placeholder',
-
-    // Íconos en la raíz para iOS
-    '/apple-touch-icon.png',
-    '/apple-touch-icon-precomposed.png',
-    '/favicon.ico',
-    
-    // Íconos de la carpeta 'icons'
-    'icons/maskable_icon.png',
-    'icons/maskable_icon_x48.png',
-    'icons/maskable_icon_x72.png',
-    'icons/maskable_icon_x96.png',
-    'icons/maskable_icon_x128.png',
-    'icons/maskable_icon_x192.png',
-    'icons/maskable_icon_x384.png',
-    'icons/maskable_icon_x512.png',
-    'icons/favicon.ico',
-
-    // Rutas de las imágenes de las cartas
-    'images/aguila.jpg',
-    'images/alacran.jpg',
-    'images/apache.jpg',
-    'images/arana.jpg',
-    'images/arbol.jpg',
-    'images/aretes.jpg',
-    'images/arpa.jpg',
-    'images/auto.jpg',
-    'images/avion.jpg',
-    'images/bailarinas.jpg',
-    'images/bandera.jpg',
-    'images/bandolon.jpg',
-    'images/barril.jpg',
-    'images/borracho.jpg',
-    'images/bota.jpg',
-    'images/botella.jpg',
-    'images/boxeadores.jpg',
-    'images/bruja.jpg',
-    'images/cacahuates.jpg',
-    'images/cafetera.jpg',
-    'images/calavera.jpg',
-    'images/camaron.jpg',
-    'images/camion.jpg',
-    'images/campana.jpg',
-    'images/cantarito.jpg',
-    'images/carretilla.jpg',
-    'images/catrin.jpg',
-    'images/cazo.jpg',
-    'images/cebolla.jpg',
-    'images/cerezas.jpg',
-    'images/chalupa.jpg',
-    'images/cisnes.jpg',
-    'images/clavel.jpg',
-    'images/cocinero.jpg',
-    'images/corazon.jpg',
-    'images/corona.jpg',
-    'images/cotorro.jpg',
-    'images/dama.jpg',
-    'images/diablito.jpg',
-    'images/domino.jpg',
-    'images/elefante.jpg',
-    'images/elote.jpg',
-    'images/escalera.jpg',
-    'images/escoba.jpg',
-    'images/escopeta.jpg',
-    'images/escuela.jpg',
-    'images/estrella.jpg',
-    'images/ferrocarril.jpg',
-    'images/fonografo.jpg',
-    'images/futbolistas.jpg',
-    'images/gallo.jpg',
-    'images/garza.jpg',
-    'images/golondrina.jpg',
-    'images/gorrito.jpg',
-    'images/guajolote.jpg',
-    'images/herradura.jpg',
-    'images/jarabe.jpg',
-    'images/jaras.jpg',
-    'images/jicara.jpg',
-    'images/jorobado.jpg',
-    'images/lagarto.jpg',
-    'images/llanta.jpg',
-    'images/luna.jpg',
-    'images/maceta.jpg',
-    'images/mandarin.jpg',
-    'images/mano.jpg',
-    'images/marimba.jpg',
-    'images/martillo.jpg',
-    'images/melon.jpg',
-    'images/motocicleta.jpg',
-    'images/muerte.jpg',
-    'images/mundo.jpg',
-    'images/muneca.jpg',
-    'images/musico.jpg',
-    'images/negrito.jpg',
-    'images/nopal.jpg',
-    'images/organo.jpg',
-    'images/oso.jpg',
-    'images/pajarito.jpg',
-    'images/palma.jpg',
-    'images/paloma.jpg',
-    'images/paracaidas.jpg',
-    'images/paraguas.jpg',
-    'images/payaso.jpg',
-    'images/pera.jpg',
-    'images/pescado.jpg',
-    'images/pinata.jpg',
-    'images/pino.jpg',
-    'images/pintor.jpg',
-    'images/plancha.jpg',
-    'images/platanos.jpg',
-    'images/puro.jpg',
-    'images/radio.jpg',
-    'images/rana.jpg',
-    'images/regadera.jpg',
-    'images/rosa.jpg',
-    'images/sandia.jpg',
-    'images/sarape.jpg',
-    'images/sirena.jpg',
-    'images/sol.jpg',
-    'images/soldado.jpg',
-    'images/tambor.jpg',
-    'images/telefono.jpg',
-    'images/tigre.jpg',
-    'images/tlachiquero.jpg',
-    'images/valiente.jpg',
-    'images/venado.jpg',
-    'images/violoncello.jpg'
+const CACHE_NAME = "loteria-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./apple-touch-icon.png",
+  "./apple-touch-icon-precomposed.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "images/aguila.png",
+  "images/alacran.png",
+  "images/apache.png",
+  "images/arana.png",
+  "images/arbol.png",
+  "images/aretes.png",
+  "images/arpa.png",
+  "images/auto.png",
+  "images/avion.png",
+  "images/bailarinas.png",
+  "images/bandera.png",
+  "images/bandolon.png",
+  "images/barril.png",
+  "images/borracho.png",
+  "images/bota.png",
+  "images/botella.png",
+  "images/boxeadores.png",
+  "images/bruja.png",
+  "images/cacahuates.png",
+  "images/cafetera.png",
+  "images/calavera.png",
+  "images/camaron.png",
+  "images/camion.png",
+  "images/campana.png",
+  "images/cantarito.png",
+  "images/carretilla.png",
+  "images/catrin.png",
+  "images/cazo.png",
+  "images/cebolla.png",
+  "images/cerezas.png",
+  "images/chalupa.png",
+  "images/cisnes.png",
+  "images/clavel.png",
+  "images/cocinero.png",
+  "images/corazon.png",
+  "images/corona.png",
+  "images/cotorro.png",
+  "images/dama.png",
+  "images/diablito.png",
+  "images/domino.png",
+  "images/elefante.png",
+  "images/elote.png",
+  "images/escalera.png",
+  "images/escoba.png",
+  "images/escopeta.png",
+  "images/escuela.png",
+  "images/estrella.png",
+  "images/ferrocarril.png",
+  "images/fonografo.png",
+  "images/futbolistas.png",
+  "images/gallo.png",
+  "images/garza.png",
+  "images/golondrina.png",
+  "images/gorrito.png",
+  "images/guajolote.png",
+  "images/herradura.png",
+  "images/jarabe.png",
+  "images/jaras.png",
+  "images/jicara.png",
+  "images/jorobado.png",
+  "images/lagarto.png",
+  "images/llanta.png",
+  "images/luna.png",
+  "images/maceta.png",
+  "images/mandarin.png",
+  "images/mano.png",
+  "images/marimba.png",
+  "images/martillo.png",
+  "images/melon.png",
+  "images/motocicleta.png",
+  "images/muerte.png",
+  "images/mundo.png",
+  "images/muneca.png",
+  "images/musico.png",
+  "images/negrito.png",
+  "images/nopal.png",
+  "images/organo.png",
+  "images/oso.png",
+  "images/pajarito.png",
+  "images/palma.png",
+  "images/paloma.png",
+  "images/paracaidas.png",
+  "images/paraguas.png",
+  "images/payaso.png",
+  "images/pera.png",
+  "images/pescado.png",
+  "images/pinata.png",
+  "images/pino.png",
+  "images/pintor.png",
+  "images/plancha.png",
+  "images/platanos.png",
+  "images/puro.png",
+  "images/radio.png",
+  "images/rana.png",
+  "images/regadera.png",
+  "images/rosa.png",
+  "images/sandia.png",
+  "images/sarape.png",
+  "images/sirena.png",
+  "images/sol.png",
+  "images/soldado.png",
+  "images/tambor.png",
+  "images/telefono.png",
+  "images/tigre.png",
+  "images/tlachiquero.png",
+  "images/valiente.png",
+  "images/venado.png",
+  "images/violoncello.png"
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Service Worker: Archivos en caché para la Lotería');
-            return Promise.all(
-                urlsToCache.map(url => {
-                    return cache.add(url).catch(err => {
-                        console.error(`Fallo al precachear el archivo: ${url}`, err);
-                    });
-                })
-            );
-        })
-    );
+// Instalación y cacheo inicial
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("📥 Archivos cacheados");
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+// Activación y limpieza de cachés viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) {
-                return response;
-            }
-            if (event.request.mode === 'navigate') {
-                return caches.match('/index.html');
-            }
-            return fetch(event.request).catch(() => {
-                return caches.match('/index.html');
-            });
-        })
-    );
-
+// Estrategia Cache First
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
+  );
 });
